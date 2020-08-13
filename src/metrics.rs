492ed8f23b5fb3cdf483d096ba9fc9153c60aea5
zero.qn <zero.qn@outlook.com>
2020-08-13 12:36:03 +0800

    feat(metrics): add message in rx gauge

--- src/metrics.rs
+++ src/metrics.rs
@@ -7,4 +7,9 @@ lazy_static! {
         "Total number of message in tx queue"
     )
     .expect("tentacle message in tx queue");
+    pub static ref TENTACLE_MESSAGE_IN_RX_QUEUE: IntGauge = register_int_gauge!(
+        "tentacle_message_in_rx_queue",
+        "Total number of message in rx queue"
+    )
+    .expect("tentacle message in rx queue");
 }
