2b9a00b6311c0080c3f75aea3f76c4ccfa022924
zero.qn <zero.qn@outlook.com>
2020-08-13 12:16:11 +0800

    feat(metric): add a gauge metric for message in tx queue

---
+++ src/metrics.rs
@@ -0,0 +1,10 @@
+use lazy_static::lazy_static;
+use prometheus::{register_int_gauge, IntGauge};
+
+lazy_static! {
+    pub static ref TENTACLE_MESSAGE_IN_TX_QUEUE: IntGauge = register_int_gauge!(
+        "tentacle_message_in_tx_queue",
+        "Total number of message in tx queue"
+    )
+    .expect("tentacle message in tx queue");
+}
